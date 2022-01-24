e8a2b1e12fe5d4f0f456fa9902237b9a2770c2af
Jeret McCoy <jeretmccoy@gmail.com>
2022-01-24 12:04:21 -0500

    Add files via upload

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+
+export default function Recent ()
+{
+    return (
+    <div className="Recent">
+        <h1>My Blog</h1>
+        <span>
+            <p>This page displays my recent posts.</p>
+        </span>
+    </div>
+    )
+}
